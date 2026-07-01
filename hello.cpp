65b1c836efaedb11437672fc773645965b378da0
user123456798-alt <mathew.abraham@g10x.com>
2026-07-01 16:24:18 +0530

    first

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
  
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+void main() {
+    string name = "ojgutdjy"
+    int age = 1235789
+    double height = 22.43
+    cout << "my name is "<<name<<", I am "<<age<< " years old, and I am "<<height<<" meters tall" << endl;
 }
