44ddb6271cd2c5e1f46ca5978ac3d0d322683507
user123456798-alt <mathew.abraham@g10x.com>
2026-07-02 12:03:52 +0530

    "first"

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-#include <string>
 using namespace std;
  
-void main() {
-    string name = "ojgutdjy"
-    int age = 1235789
-    double height = 22.43
-    cout << "my name is "<<name<<", I am "<<age<< " years old, and I am "<<height<<" meters tall" << endl;
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+void area(float* x,float* y,float* a){
+    *a = (*x) * (*y);
+}
+
+int main() {
+float l = 5.0, w = 4.0, r = 0.0;
+area(&l,&w,&r);
+std::cout<<r;
 }
